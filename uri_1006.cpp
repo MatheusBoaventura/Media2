5717d14f51605009337dd9839f6549f9ecc19657
MatheusBoaventura <70549288+MatheusBoaventura@users.noreply.github.com>
2021-01-12 20:56:28 -0300

    Create uri_1006.cpp

---
+++ uri_1006.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main (){
+    double A, B, C, media;
+
+    cin >> fixed >> setprecision(1) >> A >> B >> C;
+    
+    media = (A * 2.0 + B * 3.0 + C * 5.0) / 10;
+
+    cout << fixed << setprecision(1);
+    cout << "MEDIA = " << media << endl;
+
+    return 0;
+}
